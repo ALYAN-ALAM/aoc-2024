a995008f5abc210d642fcef5d8841e5a52daec98
ALYAN ALAM <alyanalam780@gmail.com>
2024-12-01 11:17:44 +0500

    day1

--- Day01/puzzzle1.py
+++ Day01/puzzzle1.py
@@ -7,18 +7,18 @@ def read_lists_from_file(file_path):
         for line in file:
             
             left, right = map(int, line.strip().split())
-            # print(left)
+          
             left_list.append(left)
             right_list.append(right)
     
     return left_list, right_list
 
 def calculate_total_distance(left_list, right_list):
-    # Sort both lists
+   
     left_list.sort()
     right_list.sort()
     
-    # Calculate distances
+  
     total_distance = sum(abs(l - r) for l, r in zip(left_list, right_list))
     
     return total_distance
@@ -39,14 +39,14 @@ def calculate_similarity(left_list,right_list):
     return total
         
 
-# Specify your .txt file path
+
 file_path = ".\demo_data.txt"
 
-# Read the lists
+
 left_list, right_list = read_lists_from_file(file_path)
 # print(left_list)
 
-# Calculate total distance
+
 # total_distance = calculate_total_distance(left_list, right_list)
 # print(f"Total Distance: {total_distance}")
 
